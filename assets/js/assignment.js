58b858d9971d7b82aea087becf023dd93726626a
PakoDev <92720769+pakodev43@users.noreply.github.com>
2024-08-19 17:24:47 +0500

    Add files via upload

---
+++ assets/js/assignment.js
@@ -0,0 +1,64 @@
+
+document.querySelector('[ data-meta-title ]').textContent = data.title
+document.querySelector('[ data-meta-description ]').content = data.description
+document.querySelector('[ data-meta-keywords ]').content = data.keywords
+
+document.querySelector('[ data-body-logoText ]').textContent = data.logo_text
+document.querySelector('[ data-body-main-heading-text ]').textContent = data.main_heading_text
+
+document.querySelector('[ data-link-facebook-header ]').href = data.social_media_link_facebook
+document.querySelector('[ data-link-facebook-footer ]').href = data.social_media_link_facebook
+document.querySelector('[ data-link-x-header ]').href = data.social_media_link_x
+document.querySelector('[ data-link-x-footer ]').href = data.social_media_link_x
+document.querySelector('[ data-link-instagram-header ]').href = data.social_media_link_instagram
+document.querySelector('[ data-link-instagram-footer ]').href = data.social_media_link_instagram
+
+document.querySelector('[ data-body-bio-heading ]').textContent = data.bio_heading
+document.querySelector('[ data-body-bio-para-1 ]').textContent = data.bio_paragraph_1
+document.querySelector('[ data-body-bio-para-2 ]').textContent = data.bio_paragraph_2
+document.querySelector('[ data-body-bio-para-3 ]').textContent = data.bio_paragraph_3
+
+
+
+document.querySelector('[ data-body-service-1-title ]').textContent = data.service_1_title
+document.querySelector('[ data-body-service-1-description ]').textContent = data.service_1_description
+
+document.querySelector('[ data-body-service-2-title ]').textContent = data.service_2_title
+document.querySelector('[ data-body-service-2-description ]').textContent = data.service_2_description
+
+document.querySelector('[ data-body-service-3-title ]').textContent = data.service_3_title
+document.querySelector('[ data-body-service-3-description ]').textContent = data.service_3_description
+
+document.querySelector('[ data-body-service-4-title ]').textContent = data.service_4_title
+document.querySelector('[ data-body-service-4-description ]').textContent = data.service_4_description
+
+document.querySelector('[ data-body-service-5-title ]').textContent = data.service_5_title
+document.querySelector('[ data-body-service-5-description ]').textContent = data.service_5_description
+
+document.querySelector('[ data-body-service-6-title ]').textContent = data.service_6_title
+document.querySelector('[ data-body-service-6-description ]').textContent = data.service_6_description
+
+
+
+document.querySelector('[ data-body-testimonial-1-text ]').textContent = data.testimonial_1_text
+document.querySelector('[ data-body-testimonial-1-client-name ]').textContent = data.testimonial_1_client_name
+
+document.querySelector('[ data-body-testimonial-2-text ]').textContent = data.testimonial_2_text
+document.querySelector('[ data-body-testimonial-2-client-name ]').textContent = data.testimonial_2_client_name
+
+document.querySelector('[ data-body-testimonial-3-text ]').textContent = data.testimonial_3_text
+document.querySelector('[ data-body-testimonial-3-client-name ]').textContent = data.testimonial_3_client_name
+
+document.querySelector('[ data-body-contact-section-bold-text ]').textContent = data.contact_section_bold_text
+document.querySelector('[ data-body-contact-section-text ]').textContent = data.contact_section_text
+
+
+
+document.querySelector('[ data-body-address ]').textContent = data.address
+document.querySelector('[ data-body-phone ]').textContent = data.phone
+document.querySelector('[ data-body-email ]').textContent = data.email
+document.querySelector('[ data-body-phone ]').href = "tel:" + data.phone
+document.querySelector('[ data-body-email ]').href = "mailto:" + data.email
+
+
+document.querySelector('[ data-body-copyright-name ]').textContent = data.logo_text
